17e8cbbf62d4bcbb85005b311b27ebfb96ffdff5
MelleNout <Mellenout@live.nl>
2017-09-27 14:08:55 +0200

    Added route for My Account

---
+++ app/controllers/MyAccountController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import views.html.faq.index;
+
+/**
+ * A {@link Controller} for the FAQ page.
+ * @author Melle Nout
+ * @author I.A
+ */
+public final class MyAccountController extends Controller {
+    public Result index() {
+        return ok(index.render());
+    }
+}
