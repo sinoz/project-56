8e3b1e454559464d9dea3b7abd2b4e63e54b77bd
JVDH1 <johan.hoeven@outlook.com>
2017-09-28 22:11:54 +0200

    Searchbar redirect to search results Searchbar suggestions

---
+++ public/javascripts/searchbar.js
@@ -0,0 +1,31 @@
+var a = document.createElement("OPTION");
+a.setAttribute("value", "Counter-Strike: Global Offensive");
+document.getElementById("games").appendChild(a);
+
+var b = document.createElement("OPTION");
+b.setAttribute("value", "Overwatch");
+document.getElementById("games").appendChild(b);
+
+var c = document.createElement("OPTION");
+c.setAttribute("value", "World of Warcraft");
+document.getElementById("games").appendChild(c);
+
+var d = document.createElement("OPTION");
+d.setAttribute("value", "League of Legends");
+document.getElementById("games").appendChild(d);
+
+var e = document.createElement("OPTION");
+e.setAttribute("value", "Minecraft");
+document.getElementById("games").appendChild(e);
+
+var f = document.createElement("OPTION");
+f.setAttribute("value", "PLAYERUNKNOWN'S BATTLEGROUNDS");
+document.getElementById("games").appendChild(f);
+
+var g = document.createElement("OPTION");
+g.setAttribute("value", "Destiny 2");
+document.getElementById("games").appendChild(g);
+
+var h = document.createElement("OPTION");
+h.setAttribute("value", "Hearthstone");
+document.getElementById("games").appendChild(h);
