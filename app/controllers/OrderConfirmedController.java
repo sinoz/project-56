d8c695d0911d9f47ae4c4f3d38ff70a7cf7ad9c2
MelleNout <Mellenout@live.nl>
2017-10-24 11:13:15 +0200

    Added orderconfirmed / orderfailed views to mess around in

---
+++ app/controllers/OrderConfirmedController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.orderconfirmed.index;
+
+/**
+ * A {@link Controller} for the contact page.
+ *
+ * @author Melle Nout
+ * @author I.A
+ */
+public final class OrderConfirmedController extends Controller {
+    public Result index() {
+        return ok(index.render(session()));
+    }
+}
