ba9002ee7d01c4ef3f1627557ac05b46e955ced1
MauriceVanVeen <mauricevanveenb@gmail.com>
2017-11-14 19:31:14 +0100

    Inventory product details

--- app/controllers/MyInventoryController.java
+++ app/controllers/MyInventoryController.java
@@ -14,6 +14,7 @@ import services.ProductService;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Executor;
 
@@ -133,6 +134,24 @@ public final class MyInventoryController extends Controller{
         }
 
         return out;
+    }
+
+    public CompletionStage<Result> indexProductDetails(String id) {
+        try {
+            int ID = Integer.valueOf(id);
+
+            String loggedInAs = session().get("loggedInAs");
+            if (loggedInAs == null || loggedInAs.length() == 0) {
+                return completedFuture(redirect("/login"));
+            } else {
+                Optional<Product> product = productService.fetchProduct(ID);
+                if (product.isPresent())
+                    return completedFuture(ok(views.html.inventory.details.render(product.get(), session(), "")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return completedFuture(redirect("/404"));
     }
 
 //    /**
