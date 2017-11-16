a0abdf8ab27eca3eff8136a0b4f3882b23a4f6c6
JVDH1 <johan.hoeven@outlook.com>
2017-11-16 13:15:58 +0100

    Added ProductForm

---
+++ app/forms/ProductForm.java
@@ -0,0 +1,45 @@
+package forms;
+
+import play.data.validation.Constraints;
+import play.data.validation.ValidationError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A blue print of a addProduct form.
+ *
+ * @author Johan van der Hoeven
+ */
+@Constraints.Validate
+public final class ProductForm implements Constraints.Validatable<List<ValidationError>> {
+    @Constraints.Required
+    public String title;
+
+    public String description;
+
+    @Constraints.Required
+    public boolean canBuy;
+
+    public double buyPrice;
+
+    @Constraints.Required
+    public boolean canTrade;
+
+    @Constraints.Required
+    public String passwordCurrent;
+
+    @Override
+    public List<ValidationError> validate() {
+        List<ValidationError> errors = new ArrayList<>();
+
+        if(title.length() < 6){
+            errors.add(new ValidationError("title", "The title must be at least 6 characters long."));
+        }
+
+        if(!canBuy && !canTrade){
+            errors.add(new ValidationError("canBuy", "You must at least select \"For Sale\" or \"Tradeable\"."));
+        }
+        return errors;
+    }
+}
