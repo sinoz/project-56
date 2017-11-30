2e1fa6ba98d44cc445a3d64da63b820ea9247b53
jorisstander <joris.stander@hotmail.com>
2017-11-30 18:32:06 +0100

    added review service and controller

--- app/controllers/AddReviewController.java
+++ app/controllers/AddReviewController.java
@@ -1,8 +1,16 @@
 package controllers;
 
+import forms.ReviewForm;
+import models.Review;
+import play.data.Form;
+import play.data.FormFactory;
+import play.db.Database;
 import play.mvc.Controller;
 import play.mvc.Result;
+import services.ReviewService;
 import views.html.addreview.index;
+
+import javax.inject.Inject;
 
 /**
  * A {@link Controller} for the contact page.
@@ -11,7 +19,42 @@ import views.html.addreview.index;
  *
  */
 public final class AddReviewController extends Controller {
+    /**
+     * The required {@link Database} dependency to fetch database connections.
+     */
+    private final play.db.Database database;
+    /**
+     * The {@link ReviewService} required to post reviews.
+     */
+    private final ReviewService reviews;
+
+
+    /**
+     * The required {@link FormFactory} to produce forms for modifying a user's personal settings.
+     */
+    private final FormFactory formFactory;
+
+
+    @Inject
+    public AddReviewController(FormFactory formFactory, ReviewService reviews, Database database) {
+        this.formFactory = formFactory;
+        this.reviews = reviews;
+        this.database = database;
+    }
+
     public Result index() {
-        return ok(index.render(session()));
+        return ok(views.html.addreview.index.render(formFactory.form(ReviewForm.class), session()));
+    }
+
+    public Result addReview() {
+        Form<ReviewForm> formBinding = formFactory.form(ReviewForm.class).bindFromRequest();
+        if (formBinding.hasGlobalErrors() || formBinding.hasErrors()) {
+            return badRequest(views.html.addreview.index.render(formBinding, session()));
+        } else {
+            ReviewForm form = formBinding.get();
+
+            reviews.addReview(form);
+            return redirect("/user/admin");
+        }
     }
 }
--- app/services/ReviewService.java
+++ app/services/ReviewService.java
@@ -1,4 +1,40 @@
 package services;
 
-public class ReviewService {
+import forms.ReviewForm;
+
+import javax.inject.Inject;
+import java.sql.PreparedStatement;
+
+/**
+ * TODO
+ *
+ * @author Joris Stander
+ */
+public final class ReviewService {
+
+    private final play.db.Database database;
+
+    /**
+     * Creates a new {@link ReviewService}.
+     */
+    @Inject
+    public ReviewService(play.db.Database database) {
+        this.database = database;
+    }
+
+    /**
+     * Attempts to update a exciting user and returns whether it has successfully updated the settings
+     * using the given {@link forms.ReviewForm}.
+     */
+    public void addReview(ReviewForm form) {
+        database.withConnection(connection -> {
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO reviews (userreceiverid, usersenderid, title, description, rating) VALUES(?, ?, ?, ?, ?)");
+            stmt.setString(1, "1");
+            stmt.setString(2, "2");
+            stmt.setString(3, form.Title);
+            stmt.setString(4, form.description);
+            stmt.setString(5, Integer.toString(form.rating));
+            stmt.execute();
+        });
+    }
 }
