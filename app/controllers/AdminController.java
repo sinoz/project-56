bbdc4531ce6c22215d968c712e8ead279ec7431f
MelleNout <Mellenout@live.nl>
2017-12-07 18:54:24 +0100

    Added the productsselected view, function in controller. Not Finished yet - redirects to 404, Maurice halp plox

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -1,11 +1,13 @@
 package controllers;
 
 import com.google.common.collect.Lists;
+import forms.ProductForm;
 import models.GameCategory;
 import models.Product;
 import models.User;
 import models.ViewableUser;
-import play.db.Database;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.ProductService;
@@ -22,13 +24,14 @@ import java.util.Optional;
  *
  * @author Johan van der Hoeven
  * @author Maurice van Veen
+ * @author Melle Nout
  */
 public final class AdminController extends Controller {
 
 	/**
 	 * The required {@link Database} dependency to fetch database connections.
 	 */
-	private final Database database;
+	private final play.db.Database database;
 
     /**
      * The {@link services.UserViewService} to obtain data from.
@@ -37,11 +40,15 @@ public final class AdminController extends Controller {
 
     private final ProductService productService;
 
-	@Inject
-    public AdminController(Database database, UserViewService userViewService, ProductService productService) {
+    private final FormFactory formFactory;
+
+
+    @Inject
+    public AdminController(play.db.Database database, UserViewService userViewService, ProductService productService, FormFactory formFactory) {
 	    this.database = database;
 	    this.userViewService = userViewService;
 	    this.productService = productService;
+	    this.formFactory = formFactory;
     }
 
 	public Result index() {
@@ -77,5 +84,31 @@ public final class AdminController extends Controller {
     public Result indexModifyUser(String userid){
         Optional<User> user = userViewService.fetchUser(Integer.valueOf(userid));
         return ok(modifyUser.render(session(), user));
+    }
+
+    /**
+     *  The method that renders the modify product page
+     */
+    public Result indexModifyProduct(String productid) {
+        if (SessionService.redirect(session(), database)) {
+            return redirect("/login");
+        } else {
+            try {
+                int id = Integer.valueOf(productid);
+
+                Form<ProductForm> form = formFactory.form(ProductForm.class);
+
+                String loggedInAs = SessionService.getLoggedInAs(session());
+                Optional<ViewableUser> user = userViewService.fetchViewableUser(loggedInAs);
+                Optional<Product> product = productService.fetchVisibleProduct(id);
+
+                if (product.isPresent() && user.isPresent() && product.get().getUserId() == user.get().getId()) {
+                    return ok(views.html.admin.productsSelected.render(form, product.get(), session(), "updategameaccount"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return redirect("/404");
+        }
     }
 }
